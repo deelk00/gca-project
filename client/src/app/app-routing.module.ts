889738f4daf9e7abffa20484dcbdde41cb1e6205
Elk <de.elk@Probat.com>
2022-05-24 16:14:53 +0200

    added scss classes for colors and grid

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "products/:id",
+    component: ProductDetailComponent
+  },
+  {
+    path: "",
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductOverviewComponent } from './pages/product-overview/product-overview.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductDetailComponent,
+    ProductOverviewComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
