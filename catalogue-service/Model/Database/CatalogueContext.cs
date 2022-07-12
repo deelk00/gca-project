732c9fcffa610272bf4ed2592cf30a7574010859
de.elk <de.elk@probat.com>
2022-07-12 11:49:53 +0200

    awd

--- catalogue-service/Model/Database/CatalogueContext.cs
+++ catalogue-service/Model/Database/CatalogueContext.cs
@@ -5,9 +5,6 @@ namespace CatalogueService.Model.Database
 {
     public class CatalogueContext : DbContext
     {
-<<<<<<< HEAD
-
-=======
         public DbSet<Brand> Brands { get; set; }
         public DbSet<FilterProperty> FilterProperties { get; set; }
         public DbSet<FilterPropertyDefinition> FilterPropertyDefinitions { get; set; }
@@ -35,6 +32,5 @@ namespace CatalogueService.Model.Database
         {
             base.OnModelCreating(modelBuilder);
         }
->>>>>>> 6099a1ccef0051b1269f63286f0325e7750a87ce
     }
 }
